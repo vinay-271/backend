f9a3e4a5d4d15b0f9e5b5ca671c2ab05fa8fc82e
vinay-271 <vinay.official271b@gmail.com>
2025-04-30 01:02:49 +0530

    Adding Neccessary files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "./db/index.js";
+
+// connectDB();
+/*
+import express from "express";
+const app = express();
+;(async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error", (err) => {
+            console.error("MongoDB connection error:", err);
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
+        });
+    }catch(error){
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+    }
+})()
+    */
